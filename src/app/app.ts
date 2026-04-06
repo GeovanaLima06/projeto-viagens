c185f9cc12ab60ad9bfa1fe0f7eedffabc6b75ec
GeovanaLima06 <geovanalimasoares624@gmail.com>
2026-04-06 16:55:05 -0500

    lets go

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Menu} from './menu/menu';
+import { Banner } from './banner/banner';
 import { SectionCenter } from './section-center/section-center';
 import { Encontrar } from './encontrar/encontrar';
 import { Footer } from './footer/footer';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Menu, SectionCenter, Encontrar, Footer],
+  imports: [RouterOutlet, Menu, Banner, SectionCenter, Encontrar, Footer,],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
